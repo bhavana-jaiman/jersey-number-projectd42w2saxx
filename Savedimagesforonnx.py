84cd23a7bc2315f6c483f3201672daba42c4b92f
Bhavana jaiman <bhavana202312@gmail.com>
2026-08-20 17:40:04 +0530

    Add functionality to save predicted images

---
+++ Savedimagesforonnx.py
@@ -0,0 +1,20 @@
+predicted_number = f"{digit1_prediction}{digit2_prediction}"
+
+os.makedirs("results/predicted_images", exist_ok=True)
+
+display_img = body_img.copy()
+
+cv2.putText(
+    display_img,
+    f"Predicted: {predicted_number}",
+    (5, 25),
+    cv2.FONT_HERSHEY_SIMPLEX,
+    0.7,
+    (0, 255, 0),
+    2
+)
+
+cv2.imwrite(
+    f"results/predicted_images/{os.path.basename(img)}",
+    display_img
+)
